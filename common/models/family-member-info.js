d02a8cd88b708a24980a459ae7ae1b6c3f2b5a5a
Ramon Boss <ramon.boss@outlook.com>
2016-05-18 11:25:31 +0200

    moved gender to info

--- common/models/family-member-info.js
+++ common/models/family-member-info.js
@@ -1,3 +1,6 @@
 module.exports = function(FamilyMemberInfo) {
-
+  FamilyMemberInfo.validatesInclusionOf('gender', {
+    in: ['male', 'female', 'transgender', 'other'],
+    allowNull: true
+  });
 };
--- common/models/family-member.js
+++ common/models/family-member.js
@@ -1,6 +1,3 @@
 module.exports = function(FamilyMember) {
-  FamilyMember.validatesInclusionOf('gender', {
-    in: ['male', 'female', 'transgender', 'other'],
-    allowNull: true
-  });
+
 };
