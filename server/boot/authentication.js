7d728fa81b9d14ff6c9dde5c9f68bdb66844a6ad
Ramon Boss <ramon.boss@outlook.com>
2016-06-21 15:29:00 +0200

    user is now in .env file

--- server/boot/authentication.js
+++ server/boot/authentication.js
@@ -1,4 +1,29 @@
 module.exports = function enableAuthentication(server) {
   // enable authentication
   server.enableAuth();
+
+  const User = server.models.user;
+  const Role = server.models.Role;
+  const RoleMapping = server.models.RoleMapping;
+
+  const adminUsername = process.env.STRONGLOOP_ADMIN_USER;
+  const adminPassword = process.env.STRONGLOOP_ADMIN_PASSWORD;
+
+  if(!adminUsername || !adminPassword) {
+    throw new Error('No admin user/password set. Use env STRONGLOOP_ADMIN_USER and STRONGLOOP_ADMIN_PASSWORD.');
+  }
+
+  User.create({
+    email: adminUsername, password: adminPassword
+  }, function(err, user) {
+
+    Role.create({
+      name: 'admin'
+    }, function(err, role) {
+      role.principals.create({
+        principalType: RoleMapping.USER,
+        principalId: user.id
+      }, function(err, principal) {});
+    });
+  });
 };
--- server/boot/root.js
+++ server/boot/root.js
@@ -4,22 +4,4 @@ module.exports = function(server) {
   router.get('/', server.loopback.status());
   server.use(router);
 
-  var User = server.models.user;
-  var Role = server.models.Role;
-  var RoleMapping = server.models.RoleMapping;
-
-  User.create({
-    email: 'admin@admin.admin', password: 'admin'
-  }, function(err, user) {
-
-    Role.create({
-      name: 'admin'
-    }, function(err, role) {
-      role.principals.create({
-        principalType: RoleMapping.USER,
-        principalId: user.id
-      }, function(err, principal) {});
-    });
-  });
-
 };
