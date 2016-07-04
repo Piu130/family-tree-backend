0a159b679f82114aa55019cf8466b40516cd77be
Ramon Boss <ramon.boss@outlook.com>
2016-07-04 09:52:17 +0200

    added new volume to store loopback-containers

--- server/boot/createContainers.js
+++ server/boot/createContainers.js
@@ -1,9 +1,10 @@
 module.exports = function(server) {
 
   var Container = server.models.container;
-  var folderName = 'familyMemberImages';
+  var folderName = 'family_member_images';
 
   Container.getContainer(folderName, function(err) {
+
     if (err && err.status === 404) {
       Container.createContainer({
         name: folderName,
