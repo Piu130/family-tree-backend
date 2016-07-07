8b01818c606845bd6e655e8868fcf6227c0d35ad
Ramon Boss <ramon.boss@outlook.com>
2016-07-07 15:09:49 +0200

    reduce first/lastnames to '{firstletter}*' for private persons

--- common/models/family-member.js
+++ common/models/family-member.js
@@ -1,3 +1,15 @@
 module.exports = function(FamilyMember) {
+  FamilyMember.afterRemote('find', function(context, familyMembers, next) {
+    familyMembers.map(function(familyMember) {
 
+      if (!familyMember.public) {
+        familyMember.firstNames = [familyMember.firstNames[0].charAt(0) + '*'];
+        familyMember.lastNames = [familyMember.lastNames[0].charAt(0) + '*'];
+      }
+
+      return familyMember;
+    });
+
+    next();
+  });
 };
