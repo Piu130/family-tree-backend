a9f7b264b1581ffac0909704b4dc2472c2d11381
Ramon Boss <ramon.boss@outlook.com>
2016-07-11 16:19:56 +0200

    remove names and add '{firstletter} *' instead fir private users

--- common/models/family-member.js
+++ common/models/family-member.js
@@ -1,15 +1,23 @@
 module.exports = function(FamilyMember) {
-  FamilyMember.afterRemote('find', function(context, familyMembers, next) {
-    familyMembers.map(function(familyMember) {
 
-      if (!familyMember.public) {
-        familyMember.firstNames = [familyMember.firstNames[0].charAt(0) + '*'];
-        familyMember.lastNames = [familyMember.lastNames[0].charAt(0) + '*'];
+  function reduceNamesIfNotPublic(familyMember) {
+    if (!familyMember.public && familyMember.firstNames && familyMember.lastNames) {
+      familyMember.firstNames = [familyMember.firstNames[0].charAt(0) + '*'];
+      familyMember.lastNames = [familyMember.lastNames[0].charAt(0) + '*'];
+    }
+
+    return familyMember;
+  }
+
+  FamilyMember.afterRemote('**', function(ctx, modelInstance, next) {
+    if (ctx.result) {
+      if (Array.isArray(ctx.result)) {
+        var answer = ctx.result.map(reduceNamesIfNotPublic);
+      } else {
+        var answer = reduceNamesIfNotPublic(ctx.result);
       }
-
-      return familyMember;
-    });
-
+      ctx.result = answer;
+    }
     next();
   });
 };
